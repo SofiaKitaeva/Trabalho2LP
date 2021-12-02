1f4bd337c0bf82d533fc73e923d5191b9a91080f
SofiaKitaeva <75208744+SofiaKitaeva@users.noreply.github.com>
2021-12-02 17:17:40 -0300

    Update Gari.c

--- Gari.c
+++ Gari.c
@@ -7,12 +7,11 @@ typedef struct Contador{
     struct Contador* prox;
 }contador;
 
-contador* lista = (contador*) malloc(sizeof(contador));
+contador* lista = NULL;
 
-contador* InsereCont(void* x){
-    contador* aux = (contador*) malloc(sizeof(contador));
+void* InsereCont(size_t x){
     contador* novo = (contador*) malloc(sizeof(contador));
-    novo->endereco=x;
+    novo->endereco=malloc(x);
     novo->referencias=1;
     novo->prox==NULL;
     if(lista->prox==NULL)
@@ -25,17 +24,32 @@ contador* InsereCont(void* x){
         aux->prox=novo;
     }
 
-            
+}
+
+void atualiza(void *locala, int x){
+  Contador*aux = lista;
+  while (aux!=NULL){
+      if(aux->endereco == locala){
+          aux->referencias = aux->ct+x;
+      if(aux->ct==0)
+        lista = Ct0(heap,heap->prox,heap);
+      break;}
+    aux = aux->prox;
+  }
 }
 
 void* malloc2(size_t tam){
-    
+
     void* x = malloc(tam);
-    lista=InsereCont(x);
-    
+
     return x;
 }
 
-void atrib2(int *x, int *y){
-    x = y;        
+void atrib2(int **x, int *y){
+    if(*a!=NULL){
+      atualiza(*x,-1);
+      if(b!=NULL)
+          atualiza(y,1);
+    }
+    *a=b;
 }
