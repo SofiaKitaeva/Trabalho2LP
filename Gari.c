a8fd37e0a6792c371af702440522edf7983e1d73
SofiaKitaeva <75208744+SofiaKitaeva@users.noreply.github.com>
2021-12-03 04:57:37 -0300

    Update Gari.c

--- Gari.c
+++ Gari.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include "Gari.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
 typedef struct Contador{
     void* endereco;
     int referencias;
@@ -18,48 +15,49 @@ void* InsereCont(size_t x){
     novo->endereco=malloc(x);
     novo->referencias=1;
     novo->prox==NULL;
-    if(lista->prox==NULL)
-        lista->prox=novo;
+    if(lista==NULL)
+        lista=novo;
     else{
-        contador* aux = (contador*) malloc(sizeof(contador));
-        aux=lista->prox;
+        contador* aux = lista;
         while(aux!=NULL)
             aux=aux->prox;
         aux->prox=novo;
     }
-
+    
+	return novo->endereco;
 }
 
-contador *liberamem(contador* h, contador *aux, contador *prim){
+contador *liberamem(contador* l, contador* aux, contador* prim){
   if(prim->referencias == 0){
-    free(h);
+    	free(l);
 		return aux;
   }
 	else if(aux->referencias == 0){
-		h->prox = aux->prox;
-    free(aux);
-		return h;
+		l->prox = aux->prox;
+    	free(aux);
+		return l;
 	}
 	else
-		return liberamem(h->prox,aux->prox,prim);
+		return liberamem(l->prox,aux->prox,prim);
 }
 
 void atualiza(void* x, int y){
   contador*aux = lista;
   while (aux!=NULL){
-      if(aux->endereco == x){
-          aux->referencias += y;
-      if(aux->referencias==0)
-        lista = liberamem(lista,lista->prox,lista);
-      break;}
-    aux = aux->prox;
-  }
+    	if(aux->endereco == x){
+        	aux->referencias += y;
+      		if(aux->referencias==0)
+        		lista = liberamem(lista,lista->prox,lista);
+      		break;
+		}
+    	aux = aux->prox;
+	}
 }
 
 void* malloc2(size_t tam){
-
-    void* x = malloc(tam);
-
+    
+    void* x = InsereCont(tam);
+	
     return x;
 }
 
@@ -69,5 +67,6 @@ void atrib2(int *x, int *y){
       if(y!=NULL)
           atualiza(y,1);
     }
+    
     *x=*y;
 }
